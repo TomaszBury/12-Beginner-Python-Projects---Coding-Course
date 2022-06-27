6d167cf8cb0a560db51b445feedecaecbb125bd7
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2022-06-27 19:52:10 +0200

    Madlibs done.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Course developed by Kylie Ying.
+
+# Check out her YouTube channel: https://www.youtube.com/ycubed
---
+++ madlibs.py
@@ -0,0 +1,20 @@
+# Madlibs
+# string concatenation (aka how to put strings together)
+# suppose we want to create a string that says "subscribe to ________"
+
+# some string variable
+youtuber = "Kylie Ying"
+
+# a few ways to do this:
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f" Computer programming is so {adj}! It makes me so exlited all the time because I love to {verb1}. Stay hydrated {verb2} like you are {famous_person}"
+
+print(madlib)
