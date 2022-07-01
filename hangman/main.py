32379c2321a8f871cc680cfcb7f74cf29a969300
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2022-07-01 08:46:55 +0200

    Hangman done.

--- hangman/main.py
+++ hangman/main.py
@@ -24,10 +24,13 @@ def hangman():
     # what the user has guessed
     used_letters = set()
 
+    lives = len(word)
+
     # user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         # letters used
-        print('You have used these letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left, letters used: ',
+              ' '.join(used_letters))
 
         # what current word is
         word_list = [char if char in used_letters else '_' for char in word]
@@ -38,14 +41,20 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                lives = lives - 1
+                print('letter is not in word.')
 
         elif user_letter in used_letters:
             print('You have already used that character, try again.')
         else:
             print('Invalid character. Please try again.')
 
-    # when len(word_letters) == 0
-    print(f'God game! Word: {word}')
+    # when len(word_letters) == 0 or when lives == 0
+    if lives == 0:
+        print(f'You have died, sorry not sorry. Thge word was: {word}')
+    else:
+        print(f'God game! Word: {word}')
 
 
 # user_input = input('Type something: ')
